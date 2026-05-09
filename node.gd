cd6f8ee0df98c2c82b59e8b44347662f68cd94f4
ToglivvilgoT <vilgot.johansen@hotmail.com>
2026-05-09 22:35:08 +0200

    ai path finding

--- node.gd
+++ node.gd
@@ -6,12 +6,10 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 
 func highIntense():
-	print("high")
 	intense.volume_db = 0
 	music.volume_db = -80
 
 func lowIntense():
-	print("low")
 	intense.volume_db = -80
 	music.volume_db = 0
 
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 @onready var music_player: Node = $"../Node"
+@onready var waypoints: Node = $Waypoints
 
 enum EnemyState {
 	IDLE,
@@ -9,17 +10,13 @@ enum EnemyState {
 	CHASING,
 }
 
-var direction := Vector3.FORWARD;
 var rotationTarget := Vector3.FORWARD;
-var movementTarget := Vector3.ZERO;
 var chasingTarget: Node3D = null;
+var movementTarget := position;
+var snapshotPosition := Vector3.ZERO;
 
 var state := EnemyState.IDLE;
 var stateTimer := 0.0;
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
 
 var rng := RandomNumberGenerator.new()
 func _process(delta):
@@ -34,18 +31,29 @@ func _process(delta):
 		EnemyState.CHASING:
 			_process_chasing();
 
+	if (state == EnemyState.WALKING or state == EnemyState.CHASING) and stateTimer < 0:
+		var distance = position.distance_to(snapshotPosition);
+		if (distance < 0.1):
+			_start_ideling();
+		snapshotPosition = Vector3(position);
+		stateTimer = 1;
+
 func _process_idle():
-	direction = Vector3.ZERO;
+	movementTarget = position;
 	if (stateTimer < 0):
-		_start_turning_in_random_direction();
+		_start_turning();
 
 func _process_turning():
 	var angleDiff := (transform.basis * Vector3.BACK).angle_to(rotationTarget);
 	if (angleDiff < .1):
 		_start_walking();
 
+	rotationTarget.y = 0;
+	if (rotationTarget.length() < 0.1):
+		_start_ideling();
+
 func _process_walking():
-	if (stateTimer < 0):
+	if (position.distance_to(movementTarget) < 0.1):
 		_start_ideling();
 
 func _process_chasing():
@@ -56,26 +64,33 @@ func _process_chasing():
 		return;
 
 	rotationTarget = chasingTarget.position - position;
-	direction = rotationTarget.normalized();
+	movementTarget = chasingTarget.position;
 
 func _start_ideling():
 	state = EnemyState.IDLE;
-	stateTimer = 2;
+	stateTimer = 2.0;
+	music_player.lowIntense();
 
-func _start_turning_in_random_direction():
-	var rotationAngle := rng.randf() * TAU;
-	rotationTarget = Vector3.FORWARD.rotated(Vector3.UP, rotationAngle);
+func _start_turning():
+	while true:
+		var points := waypoints.get_children();
+		movementTarget = points[rng.randi_range(0, points.size() - 1)].position;
+		if (movementTarget.distance_to(position) > 0.1):
+			break;
+	rotationTarget = (movementTarget - position);
 	state = EnemyState.TURNING;
 
 func _start_walking():
-	direction = (transform.basis * Vector3(0, 0, 1)).normalized();
-	stateTimer = 3;
 	state = EnemyState.WALKING;
+	snapshotPosition = position;
+	stateTimer = 1.0;
 
 func _start_chasing(body):
 	chasingTarget = body;
 	music_player.highIntense();
 	state = EnemyState.CHASING;
+	snapshotPosition = position;
+	stateTimer = 1.0;
 
 func _handle_collision(body):
 	if body.is_in_group("player") && state == EnemyState.CHASING:
@@ -101,6 +116,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	var direction = (movementTarget - position).normalized();
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
@@ -111,4 +127,5 @@ func _physics_process(delta: float) -> void:
 	var rotationAngle = rotationTarget.signed_angle_to(basis * Vector3.FORWARD, Vector3.UP);
 	rotate(Vector3.UP, sign(rotationAngle) * rotation_speed * delta);
 
-	move_and_slide()
+	if (state == EnemyState.CHASING or state == EnemyState.WALKING):
+		move_and_slide()
