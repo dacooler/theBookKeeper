861fe02ee031451d988ca287068cde132c45e141
dacoolest <oscar.jansson1@proton.me>
2026-05-09 16:03:26 +0200

    hej

---
+++ node.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@onready var music = $AudioStreamPlayer
+@onready var intense = $AudioStreamPlayer2
+
+# Called when the node enters the scene tree for the first time.
+func start() -> void:
+	music.play()
+	intense.play()
+	
+func highIntense():
+	intense.volume_db = 0
+	music.volume_db = -80
+	
+func lowIntense():
+	intense.volume_db = -80
+	music.volume_db = 0
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
