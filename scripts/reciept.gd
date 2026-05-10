62af722d810b045073529f3a3b02041ea6604663
ToglivvilgoT <vilgot.johansen@hotmail.com>
2026-05-10 14:27:35 +0200

    more variety to reciepts

--- scripts/reciept.gd
+++ scripts/reciept.gd
@@ -1,10 +1,34 @@
 extends Area3D
 
+var startY := position.y;
+var time := 0.0;
+
+const textures = [
+	preload("res://assets/kvitto1.png"),
+	preload("res://assets/kvitto2.png"),
+	preload("res://assets/kvitto3.png"),
+	preload("res://assets/kvitto4.png")
+]
+@onready var mesh: MeshInstance3D = $MeshInstance3D;
+
 func _ready():
-	await get_tree().create_timer(60.0).timeout
-	queue_free()
+	var mat := StandardMaterial3D.new();
+	mat.albedo_texture = textures.pick_random();
+	mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+	mat.cull_mode = BaseMaterial3D.CULL_DISABLED
+	mesh.material_override = mat;
+	await get_tree().create_timer(60.0).timeout;
+	queue_free();
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		queue_free();
-		body.collect_recipt();
+		body.collect_recipt(mesh.get_active_material(0));
+
+func _process(delta: float):
+	time += delta;
+
+	const BOBBINESS := 0.3;
+	position.y = startY + BOBBINESS + sin(time * 2) * BOBBINESS;
+
+	rotate(Vector3.UP, delta);
