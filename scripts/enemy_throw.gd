f8fea6a00af6c20c716360e78e9428273919b8b0
ToglivvilgoT <vilgot.johansen@hotmail.com>
2026-05-10 11:36:01 +0200

    pushang tables better but not good enoght

--- scripts/enemy_throw.gd
+++ scripts/enemy_throw.gd
@@ -5,5 +5,6 @@ func _on_body_entered(body: Node3D):
 	if body.is_in_group("throw"):
 		var rigidBody: RigidBody3D = body;
 		var direction = (body.position - position).normalized();
+		direction.y = 0.5;
 		rigidBody.linear_velocity += direction * 10;
 		print_debug("hallD")
